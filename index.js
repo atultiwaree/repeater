abc3253f453ef1b6e87be4a35dac9c811a34dd82
Atul Tiwaree <atul@giniedigital.com>
2024-05-23 18:30:55 +0530

    Flow completed

--- index.js
+++ index.js
@@ -1,46 +1,99 @@
-const { Builder, By  } = require("selenium-webdriver");
+const { Builder, By, WebElement, Key  } = require("selenium-webdriver");
 
 
 
 const startEngine = async () => {
 
-        //Create driver instance
-        const driver = await new Builder().forBrowser('chrome').build()
+       try {
 
-        //Open website
-        await driver.get('https://instagram.com');
+         //Create driver instance
+         const driver = await new Builder().forBrowser('chrome').build()
 
-        //Find username input field
-        const userNameField = await driver.findElement(By.name("username"))
+         //Make full screen
+         await driver.manage().window().maximize()
+ 
+ 
+ 
+         //Open website
+         await driver.get('https://instagram.com');
+ 
+         //Find username input field
+         const userNameField = await driver.findElement(By.name("username"))
+ 
+         userNameField.sendKeys("artistic__vinci")
+         
+ 
+         //Find password input field
+         const passwordField = await driver.findElement(By.name("password"))
+ 
+         passwordField.sendKeys("Qwerty@1234")
+ 
+ 
+         //Press Login button
+         const loginButton = await driver.findElement(By.xpath("//form[@id='loginForm']/div/div[3]"));
+ 
+         await loginButton.click()
+ 
+         
+         await driver.sleep(5000)
+ 
+         //Press Saveinfo
+         await driver.findElement(By.xpath("//button[contains(.,'Save info')]")).click()
+ 
+         await driver.sleep(3000)
+ 
+         //Notification
+        await driver.findElement(By.xpath("//button[contains(.,'Not Now')]")).click()
+ 
+         await driver.sleep(1000)
+ 
+         //click on search
+ 
+         const search = await driver.findElement(By.xpath("//div[2]/span/div/a/div/div[2]/div/div/span/span")).click()
 
-        userNameField.sendKeys("artistic__vinci")
+         //Now paste user name
+
+         await driver.switchTo().activeElement().sendKeys("rii_shabh_")
+
+         await driver.sleep(2000)
+
+         //Click on first result
+         
+         await driver.switchTo().activeElement().sendKeys(Key.DOWN)
+
+         await driver.sleep(2000)
+         
+         await driver.switchTo().activeElement().sendKeys(Key.ENTER)
+         
+         await driver.sleep(3000)
+
+         //Click on message button
+
+         let message = await driver.findElement(By.xpath("//div[2]/div/div[2]/div")).click();
+
+         await driver.sleep(3000)
+
+         await driver.switchTo().activeElement().sendKeys("Yo yo", Key.ENTER)
+
         
 
-        //Find password input field
-        const passwordField = await driver.findElement(By.name("password"))
-
-        passwordField.sendKeys("Qwerty@1234")
 
 
-        //Press Login button
-        const loginButton = await driver.findElement(By.xpath("//form[@id='loginForm']/div/div[3]"));
+         
+         
+ 
+ 
+ 
+        //        driver.executeScript("window.scroll(0, 500);").then((e) => console.log("Trx window scroll"));
 
-        await loginButton.click()
-
-        
-        await driver.sleep(3000)
-
-        //Press Saveinfo
-        const saveInfoButton = await driver.findElement(By.xpath("//button[contains(.,'Save info')]")).click()
-
-        await driver.sleep(3000)
-
-        //Notification
-        const notificationButton = await driver.findElement(By.xpath("//button[contains(.,'Not Now')]")).click()
 
         
 
+        
 
+       } catch(e) {
+           console.log(e.message)
+       }
         
 }
 
